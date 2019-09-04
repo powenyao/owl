69752423711f2dd9572ffce40b5c573f41623350
KevinGit1118 <31171132+KevinGit1118@users.noreply.github.com>
2019-09-03 21:20:44 -0700

    No need complex camera for now, make a place holder object and put comment to point out some scalable structure

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  Basic CameraManager
+ *  Before finding any better method, for control player camera
+ *  Use this to link both player transform and assigned camera
+ *  For different camera function, extend this class to use module
+ *  And create each module as different camera funcitonality so we can easily test different funcitons
+ */
+public class CameraManager : MonoBehaviour
+{
+    public Camera l_camera;
+    public Transform l_targetTransform;
+
+    void Awake()
+    {
+        if(!l_camera)
+        {
+            Debug.LogError("[CameraManager] Camera is none!");
+        }
+
+        if(!l_targetTransform)
+        {
+            Debug.LogError("[CameraManager] Target is none!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // follow player position for now
+        l_camera.transform.position = new Vector3(l_targetTransform.position.x, l_targetTransform.position.y, l_camera.transform.position.z);
+    }
+}
